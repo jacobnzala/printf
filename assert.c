52398617bd738ff0c80da40e6380001faf024d53
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-17 19:49:58 +0200

    Update assert.c

--- assert.c
+++ assert.c
@@ -6,13 +6,13 @@
  */
 _Bool invalidInputs(const char *p)
 {
-	/* NULL */
+	
 	if (!p)
 		return (false);
-	/* "%" */
+	
 	if (*p == '%' && !*(p + 1))
 		return (false);
-	/* "% " */
+	
 	if (*p == '%' && *(p + 1) == ' ' && !*(p + 2))
 		return (false);
 	return (true);
