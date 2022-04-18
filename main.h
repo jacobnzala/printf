ea3546c88f6d3af9b414940bd4e14dd42b249d4a
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-18 20:48:49 +0200

    Update main.h

--- main.h
+++ main.h
@@ -13,7 +13,7 @@
 #define HEXA "0x"
 #define NULL_STRING "(null)"
 #define NUL '\0'
-#define KILO 1024
+#define KILOBYTE 1024
 #define ABS(x) (((x) <  0) ? -(x) : (x))
 
 /**
@@ -28,7 +28,7 @@ typedef struct modifiers
 	_Bool plus;
 	_Bool space;
 	_Bool hash;
-} jacs;
+} mods;
 
 /**
  * struct printHandler - struct to choose the right function depending
@@ -39,20 +39,20 @@ typedef struct modifiers
 typedef struct printHandler
 {
 	char c;
-	int (*f)(va_list ap, jacs *f);
+	int (*f)(va_list ap, mods *f);
 } ph;
 
 /* print_nums */
-int print_int(va_list l, jacs *f);
+int print_int(va_list l, mods *f);
 void print_number(int n);
-int print_unsigned(va_list l, jacs *f);
+int print_unsigned(va_list l, mods *f);
 int count_digit(int i);
 
 /* print_bases */
-int print_hex(va_list l, jacs *f);
-int print_hex_big(va_list l, jacs *f);
-int print_binary(va_list l, jacs *f);
-int print_octal(va_list l, jacs *f);
+int print_hex(va_list l, mods *f);
+int print_hex_big(va_list l, mods *f);
+int print_binary(va_list l, mods *f);
+int print_octal(va_list l, mods *f);
 
 /* converter */
 char *convert(unsigned long int num, int base, int lowercase);
@@ -61,30 +61,30 @@ char *convert(unsigned long int num, int base, int lowercase);
 int _printf(const char *format, ...);
 
 /* get_print */
-int (*get_print(char s))(va_list, jacs *);
+int (*get_print(char s))(va_list, mods *);
 
 /* get_flags */
-int get_flags(char s, jacs *f);
+int get_flags(char s, mods *f);
 
 /* print_alpha */
-int print_string(va_list l, jacs *f);
-int print_char(va_list l, jacs *f);
+int print_string(va_list l, mods *f);
+int print_char(va_list l, mods *f);
 
 /* write_funcs */
 int _putchar(char c);
 int _puts(char *str);
 
 /* print_custom */
-int print_rot13(va_list l, jacs *f);
-int print_rev(va_list l, jacs *f);
-int print_bigS(va_list l, jacs *f);
+int print_rot13(va_list l, mods *f);
+int print_rev(va_list l, mods *f);
+int print_bigS(va_list l, mods *f);
 _Bool isNonAlphaNumeric(char c);
 
 /* print_address */
-int print_address(va_list l, jacs *f);
+int print_address(va_list l, mods *f);
 
 /* print_percent */
-int print_percent(va_list l, jacs *f);
+int print_percent(va_list l, mods *f);
 
 /* assertions */
 _Bool invalidInputs(const char *p);
